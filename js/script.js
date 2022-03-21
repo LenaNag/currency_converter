d39731ed94cfd0992cef9239487ea9037806c620
Alena Nahornaya <nahornaya.alena@gmail.com>
2022-03-21 22:08:26 +0100

    init function created

--- js/script.js
+++ js/script.js
@@ -12,19 +12,22 @@
         }
     };
 
-    const formElement = document.querySelector(".js-form");
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
 
-    formElement.addEventListener("submit", (event) => {
-        event.preventDefault();
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
 
-        const amountElement = document.querySelector(".js-form__field-amount");
-        const currencyElement = document.querySelector(".js-form__field-currency");
-        const resultElement = document.querySelector(".js-form__result");
+            const amountElement = document.querySelector(".js-form__field-amount");
+            const currencyElement = document.querySelector(".js-form__field-currency");
+            const resultElement = document.querySelector(".js-form__result");
 
-        const amount = +amountElement.value;
-        const currency = currencyElement.value;
-        let result = calculateResult(amount, currency);
+            const amount = +amountElement.value;
+            const currency = currencyElement.value;
+            let result = calculateResult(amount, currency);
 
-        resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency}`;
-    });
+            resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency}`;
+        });
+    };
+    init();
 }
